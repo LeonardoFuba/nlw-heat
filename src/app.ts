9ce8c7b66942dca12fa325ec21edeecdc8632456
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-21 19:52:32 -0300

    dev: web socket connection

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+})
+
 app.use(express.json());
 app.use(router);
 
@@ -17,4 +30,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code)
 })
 
-app.listen(4000, () => console.log('server is running on PORT 4000'));
+export { serverHttp, io };
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../prisma";
+import { io } from "../app";
 
 const execute = async (text: string, user_id: string) => {
   const message = await prismaClient.message.create({
@@ -10,6 +11,18 @@ const execute = async (text: string, user_id: string) => {
       user: true,
     },
   });
+
+  const infoWS = {
+    text: message.text,
+    user_id: message.user_id,
+    created_at: message.created_at,
+    user: {
+      name: message.user.name,
+      avatar_url: message.user.avatar_url
+    }
+  }
+
+  io.emit("new_message", infoWS);
 
   return message;
 }
