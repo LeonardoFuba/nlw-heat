a4b29dff42a16b919f99683982d4853d6f445816
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-23 20:00:01 -0300

    dev: listening socket io new messages

--- nlwheatapp/src/components/MessageList/index.tsx
+++ nlwheatapp/src/components/MessageList/index.tsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
 import { Message, MessageProps } from '../Message'
 import { styles } from './styles';
 
+const messagesQueue: MessageProps[] = [];
+const socket = io(String(api.defaults.baseURL));
+
+socket.on('new_message', (newMessage: MessageProps) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList(){
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000)
+  }, []);
 
   useEffect(() => {
     async function fetchMessages(){
