d55740e6ecca585e1f841ce7bcee59397e6cd426
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2021-10-23 14:12:19 -0300

    dev: send message form and buttons

--- nlwheatapp/src/screens/Home/index.tsx
+++ nlwheatapp/src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
-
 import { Header } from '../../components/Header'
 import { MessageList } from '../../components/MessageList'
+import { SignInBox } from '../../components/SignInBox';
+import { SendMessageForm } from '../../components/SendMessageForm';
 
 import { styles } from './styles';
 
@@ -11,6 +12,8 @@ export function Home(){
     <View style={styles.container}>
       <Header />
       <MessageList />
+      {/* <SignInBox /> */}
+      <SendMessageForm />
     </View>
   );
 }
